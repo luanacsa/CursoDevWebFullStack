3cf4288b820ff192320a720d62835a7abf9b5c84
Luana Araújo <luanacsaraujo@gmail.com>
2021-10-27 17:46:08 -0300

    HTML, CSS e Javascript juntos.

--- Modulo1/code/2HTML_CSS_Javascript_juntos/script.js
+++ Modulo1/code/2HTML_CSS_Javascript_juntos/script.js
@@ -1,1 +1,16 @@
+let show = false;
 
+let list = document.getElementById("list");
+
+function showHide(e){
+    console.log(e)
+    if (show) {
+        show = false;
+        list.style.display = 'none'; 
+        e.innerHTML = "show";
+    }else{
+        show = true;
+        list.style.display = 'block'; 
+        e.innerHTML = "hide";
+    }
+}
